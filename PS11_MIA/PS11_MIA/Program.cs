edec3b811ca8a08902ce52446c74dc55d1b3de2e
viax30 <inteviamarie@gmail.com>
2019-01-09 14:36:05 +0800

    Problem Set #11

---
+++ PS11_MIA/PS11_MIA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PS11_MIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100,prev = 0, current = 1,tmp;
+            Console.Write("First 100 of Fibonacci Sequence");
+            Console.Write("{0} {1}", prev, current);
+            // Fibonacci Process
+            for (int i = 2; i < n; i++)
+            {
+                tmp = prev + current;
+                Console.Write(" {0} ", tmp);
+                prev = current;
+                current= tmp;
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}

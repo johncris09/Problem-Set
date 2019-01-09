98cd875a5d1512c052089c6384306149ac9aecee
viax30 <inteviamarie@gmail.com>
2019-01-09 14:35:59 +0800

    Problem Set #10

---
+++ PS10_MIA/PS10_MIA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PS10_MIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter an integer:");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

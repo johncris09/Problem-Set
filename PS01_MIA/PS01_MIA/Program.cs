2d73df6cffe4f41772aa5129098c212f4d4dcce0
viax30 <inteviamarie@gmail.com>
2019-01-09 14:33:56 +0800

    Problem Set #1

---
+++ PS01_MIA/PS01_MIA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PS01_MIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fnum, snum, tnum, sum;
+
+            // Inputs
+            Console.Write("Finding the sum of 3 numbers\n");
+            Console.Write("Enter first number: ");
+            fnum = double.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            snum = double.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            tnum = double.Parse(Console.ReadLine());
+
+            // Process
+            sum = fnum + snum + tnum;
+
+            // Output
+
+            Console.WriteLine("Total: {0} " , sum);
+
+            Console.ReadLine();
+        }
+    }
+}

3df46fdad7dac56edd76e531f086a2af6c512eab
viax30 <inteviamarie@gmail.com>
2019-01-09 14:35:34 +0800

    Problem Set #8

---
+++ PS08_MIA/PS08_MIA/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PS08_MIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Size of an Array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] numbers = new int[size];
+            Console.WriteLine("Enter the values of the array:");
+            for (int i = 0; i < numbers.Length; i++) {
+                Console.Write("Enter the "+ (i+1) +" integer: ");
+                numbers[i] = (int) int.Parse(Console.ReadLine());
+            }
+
+            
+            // Sorting Process
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                // Loop operating over the unsorted part of the array   
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    // Swapping the values    
+                    if (numbers[i] > numbers[j])
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            } // End of the sorting logic
+            
+            
+            Console.WriteLine("Largest Number: " + numbers[numbers.Length-1]);
+            
+  
+
+            Console.ReadLine();
+        }
+
+    }
+}

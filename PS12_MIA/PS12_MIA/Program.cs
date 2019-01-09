d6d16c5189dfd597bf3f4dc29e2c966037d2f167
viax30 <inteviamarie@gmail.com>
2019-01-09 14:36:14 +0800

    Problem Set #12

---
+++ PS12_MIA/PS12_MIA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PS12_MIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            double sum = 1;
+            sum += (1 / 2);
+            for (int i = 0; i < 100; i++ )
+            {
+                if (i % 2 == 0)
+                {
+                    sum += sum/(num+i+1);
+                    Console.WriteLine("{0:0.00}", sum);
+                   
+                }
+                else 
+                {
+                    sum -= sum / (num + i + 1);
+                    Console.WriteLine("{0:0.00}", sum);
+                }
+
+
+            }
+            Console.WriteLine("{0:0.00}",sum);
+            Console.ReadLine();
+        }
+    }
+}
